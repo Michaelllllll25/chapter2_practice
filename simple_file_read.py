ee2b39356c251b2a60ad1c3f119bbe43512b6582
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-06 19:23:45 -0400

    Create simple_file_read.py

---
+++ simple_file_read.py
@@ -0,0 +1,7 @@
+with open("name.txt", "w") as f:
+    f.write("Horace\n")
+    f.write("Mann")
+
+with open("name.txt", "w") as f:
+    f.write("Michael\n")                # Replaces the above txt
+    f.write("Wilson")
