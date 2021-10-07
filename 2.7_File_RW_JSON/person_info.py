afee917436c32efbe0103ba89bc7270f2e050886
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-06 21:01:10 -0400

    Create person_info.py

---
+++ 2.7_File_RW_JSON/person_info.py
@@ -0,0 +1,11 @@
+# A dictionary holding info about
+# a particular person
+person = {
+    "name": "Johnny Smitherson",
+    "age": 54,
+    "hair_color": "blonde"
+}
+
+print(f"The person's name is {person['name']}.")
+print(f"They are {person['age']} years old.")
+print(f"Their hair is {person['hair_color']}.")
