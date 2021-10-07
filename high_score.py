f36ee6f8c0b84a4b950af326dd684e645f81b1c4
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-06 20:56:03 -0400

    Delete high_score.py

--- high_score.py
+++
@@ -1,7 +0,0 @@
-score = str(input("Please enter your score: "))
-name = input("Please enter your name: ")
-
-with open("score.txt", "w") as f:
-    f.write(score)
-    f.write(" \n")
-    f.write(name)
