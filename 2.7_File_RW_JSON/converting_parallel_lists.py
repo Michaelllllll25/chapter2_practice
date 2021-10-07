afb569e0a408fe999c21b093ce2179eebf235bfc
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-07 18:47:46 -0400

    Create converting_parallel_lists.py

---
+++ 2.7_File_RW_JSON/converting_parallel_lists.py
@@ -0,0 +1,83 @@
+Mitchell = {
+    "name": "Mitchell",
+    "average": 99.5,
+    "student_id": 123456
+}
+
+Ortiz = {
+    "name": "Ortiz",
+    "average": 78.5,
+    "student_id": 813225
+}
+
+Luu = {
+    "name": "Luu",
+    "average": 95.6,
+    "student_id": 823669
+}
+
+Zimmerman = {
+    "name": "Zimmerman",
+    "average": 96.8,
+    "student_id": 307760
+}
+
+Brooks = {
+    "name": "Brooks",
+    "average": 82.7,
+    "student_id": 827131
+}
+
+while True:
+    print("[1] Yes")
+    print("[2] No")
+    con = int(input("Do you want to search another name: "))
+    if con == 1:
+        print("Bye")
+        break
+
+    print()
+    print("[1] Mitchell")
+    print("[2] Ortiz")
+    print("[3] Luu")
+    print("[4] Zimmerman")
+    print("[5] Brooks")
+    print()
+
+    name = int(input("Enter name: "))
+
+
+    if name == 1:
+        print()
+        print(f"Name: {Mitchell['name']}")
+        print(f"Average: {Mitchell['average']}")
+        print(f"Student ID: {Mitchell['student_id']}")
+        print()
+
+    elif name == 2:
+        print()
+        print(f"Name: {Ortiz['name']}")
+        print(f"Average: {Ortiz['average']}")
+        print(f"Student ID: {Ortiz['student_id']}")
+        print()
+
+    elif name == 3:
+        print()
+        print(f"Name: {Luu['name']}")
+        print(f"Average: {Luu['average']}")
+        print(f"Student ID: {Luu['student_id']}")
+        print()
+
+    elif name == 4:
+        print()
+        print(f"Name: {Zimmerman['name']}")
+        print(f"Average: {Zimmerman['average']}")
+        print(f"Student ID: {Zimmerman['student_id']}")
+        print()
+
+    elif name == 5:
+        print()
+        print(f"Name: {Brooks['name']}")
+        print(f"Average: {Brooks['average']}")
+        print(f"Student ID: {Brooks['student_id']}")
+        print()
