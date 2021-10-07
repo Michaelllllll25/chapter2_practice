d749f3a05e27a6dd577a41e71ebc90994497e659
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-07 10:51:33 -0400

    Create converting_a_list.py

---
+++ 2.7_File_RW_JSON/converting_a_list.py
@@ -0,0 +1,11 @@
+person = {
+    "name": "Jim",
+    "color": "blue",
+    "number": 54,
+    "other_color": "green"
+}
+
+print(f"Name: {person['name']}")
+print(f"Color: {person['color']}")
+print(f"Number: {person['number']}")
+print(f"Other Color: {person['other_color']}")
