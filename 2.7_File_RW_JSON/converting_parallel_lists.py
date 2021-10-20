0604c7f6387e35911afacfaedeb35f41f3b4e0a2
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-19 20:24:37 -0400

    Update converting_parallel_lists.py

--- 2.7_File_RW_JSON/converting_parallel_lists.py
+++ 2.7_File_RW_JSON/converting_parallel_lists.py
@@ -1,103 +1,32 @@
-values = [
-    'Mitchell', '99.5', '123456'
-    'Ortiz', '78.5', '813225'
-    'Luu', '95.6', '823669'
-    'Zimmerman', '96.8', '307760'
-    'Brooks', '82.7', '827131'
-    ]
-
+name = {
+    '123456': 'Mitchell 99.5',
+    '813225': 'Ortiz 78.5', 
+    '823669': 'Luu 95.6', 
+    '307760': 'Zimmerman 96.8', 
+    '827131': 'Brooks 82.7'
+}
 
 
 
 search = input("What ID number do you want to find? ")
 
-if search == values[3]:
-    print(values[1])
+if search == "123456":
+    print("ID: 123456")
+    print(name["123456"])
 
+elif search == "813225":
+    print("ID: 813225")
+    print(name["813225"])
 
+elif search == "823669":
+    print("ID: 823669")
+    print(name["823669"])
 
+elif search == "307760":
+    print("ID: 307760")
+    print(name["307760"])
 
+elif search == "827131":
+    print("ID: 827131")
+    print(name["827131"])
 
-Mitchell = {
-    "name": "Mitchell",
-    "average": 99.5,
-    "student_id": 123456
-}
-
-Ortiz = {
-    "name": "Ortiz",
-    "average": 78.5,
-    "student_id": 813225
-}
-
-Luu = {
-    "name": "Luu",
-    "average": 95.6,
-    "student_id": 823669
-}
-
-Zimmerman = {
-    "name": "Zimmerman",
-    "average": 96.8,
-    "student_id": 307760
-}
-
-Brooks = {
-    "name": "Brooks",
-    "average": 82.7,
-    "student_id": 827131
-}
-
-while True:
-    print("[1] Yes")
-    print("[2] No")
-    con = int(input("Do you want to search another name: "))
-    if con == 1:
-        print("Bye")
-        break
-
-    print()
-    print("[1] Mitchell")
-    print("[2] Ortiz")
-    print("[3] Luu")
-    print("[4] Zimmerman")
-    print("[5] Brooks")
-    print()
-
-    name = int(input("Enter name: "))
-
-
-    if name == 1:
-        print()
-        print(f"Name: {Mitchell['name']}")
-        print(f"Average: {Mitchell['average']}")
-        print(f"Student ID: {Mitchell['student_id']}")
-        print()
-
-    elif name == 2:
-        print()
-        print(f"Name: {Ortiz['name']}")
-        print(f"Average: {Ortiz['average']}")
-        print(f"Student ID: {Ortiz['student_id']}")
-        print()
-
-    elif name == 3:
-        print()
-        print(f"Name: {Luu['name']}")
-        print(f"Average: {Luu['average']}")
-        print(f"Student ID: {Luu['student_id']}")
-        print()
-
-    elif name == 4:
-        print()
-        print(f"Name: {Zimmerman['name']}")
-        print(f"Average: {Zimmerman['average']}")
-        print(f"Student ID: {Zimmerman['student_id']}")
-        print()
-
-    elif name == 5:
-        print()
-        print(f"Name: {Brooks['name']}")
-        print(f"Average: {Brooks['average']}")
-        print(f"Student ID: {Brooks['student_id']}")
-        print()
